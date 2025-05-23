2278ace7199e9d05053f98b39c5a9fee61507bc5
KuizW9 <kuiz2sw@gmail.com>
2025-05-23 23:58:25 +0800

    Update run_offset.sh

--- run_offset.sh
+++ run_offset.sh
@@ -5,11 +5,15 @@ export NEZHA_PORT=${NEZHA_PORT:-''}           # v1哪吒不要填写这个,v0哪
 export NEZHA_KEY=${NEZHA_KEY:-''}             # v1的NZ_CLIENT_SECRET或v0的agent密钥
 export ARGO_DOMAIN=${ARGO_DOMAIN:-'app-test.alvgw.xyz'}         # 固定隧道域名,留空即启用临时隧道
 export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiMWEwMjRhODEtZGMwYy00MjNjLWEzMGYtNDJjOTQ2NGY4NDc3IiwicyI6IlkyVmpaRFV6TWpVdFpqa3pOUzAwTnprMkxUZzBOemN0T0RKaE0yRmhPREpsTWpFMCJ9'}             # 固定隧道token或json,留空即启用临时隧道
+#export ARGO_DOMAIN=${ARGO_DOMAIN:-'app-ru.alvgw.xyz'}         # 固定隧道域名,留空即启用临时隧道
+#export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiMWEwMjRhODEtZGMwYy00MjNjLWEzMGYtNDJjOTQ2NGY4NDc3IiwicyI6IlkyVmpaRFV6TWpVdFpqa3pOUzAwTnprMkxUZzBOemN0T0RKaE0yRmhPREpsTWpFMCJ9'}             # 固定隧道token或json,留空即启用临时隧道
+
 export CFIP=${CFIP:-'www.visa.com.tw'}        # argo节点优选域名或优选ip
 export CFPORT=${CFPORT:-'443'}                # argo节点端口 
 export NAME=${NAME:-'Vls'}                    # 节点名称  
 export FILE_PATH=${FILE_PATH:-'./.cache'}     # sub 路径  
 export ARGO_PORT=${ARGO_PORT:-'54322'}         # argo端口 使用固定隧道token,cloudflare后台设置的端口需和这里对应
+#export ARGO_PORT=${ARGO_PORT:-'54321'}         # argo端口 使用固定隧道token,cloudflare后台设置的端口需和这里对应
 export TUIC_PORT=${TUIC_PORT:-'40000'}        # Tuic 端口，支持多端口玩具可填写，否则不动
 export HY2_PORT=${HY2_PORT:-'50000'}          # Hy2 端口，支持多端口玩具可填写，否则不动
 export REALITY_PORT=${REALITY_PORT:-'60000'}  # reality 端口,支持多端口玩具可填写，否则不动   
