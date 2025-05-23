2fc22fb47b333137bb17d324dcd445d46b8f33f7
KuizW9 <kuiz2sw@gmail.com>
2025-05-24 06:31:37 +0800

    Update cf.sh

--- app-argo/cf.sh
+++ app-argo/cf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #app-argo-{ports} #54321, 7000-7002
+
+
+curl -L -o ./cloudflared -# --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64
+chmod +x ./cloudflared 
 export ARGO_AUTH=${agk:-'eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiZGJiNzhkYzEtZGZmMC00NDI4LTk4OGUtOTA4NWFhODhhMTZlIiwicyI6Ik0yUmhPRFpqTlRJdE1qVXlOeTAwTURFNExXSmlObVl0TVdNeVpqazBOekl6TTJZMiJ9'}
 nohup ./cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH} >/dev/null 2>&1 & echo "$!" > ./sbargopid.log &&
 echo ${ARGO_AUTH} > ./sbargotoken.log
