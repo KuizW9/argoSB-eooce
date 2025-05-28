87bd2e13a1fcb28b8129d09dc861037399de2de0
KuizW9 <kuiz2sw@gmail.com>
2025-05-29 04:11:50 +0800

    Update cf.sh

--- app-argo/cf.sh
+++ app-argo/cf.sh
@@ -2,9 +2,12 @@
 #app-argo-{ports} #54321, 7000-7002
 
 
-curl -L -o ./cloudflared -# --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64
+curl -L -o ./cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64
 chmod +x ./cloudflared 
-export ARGO_AUTH=${agk:-'eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiZGJiNzhkYzEtZGZmMC00NDI4LTk4OGUtOTA4NWFhODhhMTZlIiwicyI6Ik0yUmhPRFpqTlRJdE1qVXlOeTAwTURFNExXSmlObVl0TVdNeVpqazBOekl6TTJZMiJ9'}
+
+#idx.alvgw.xyz
+#cloudflared tunnel run --token eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiOWYwZWZhYmEtOGJjOC00YmM4LThkMTEtYmIzMDY1NmIzODQ1IiwicyI6Ik5qSXpORE0wT1RVdE0yUTNOUzAwWTJFekxXSTNPR1F0TWpSaE1EVTBaVFExWldJeSJ9
+export ARGO_AUTH=${agk:-'eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiOWYwZWZhYmEtOGJjOC00YmM4LThkMTEtYmIzMDY1NmIzODQ1IiwicyI6Ik5qSXpORE0wT1RVdE0yUTNOUzAwWTJFekxXSTNPR1F0TWpSaE1EVTBaVFExWldJeSJ9'}
 nohup ./cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH} >/dev/null 2>&1 & echo "$!" > ./sbargopid.log &&
 echo ${ARGO_AUTH} > ./sbargotoken.log
 echo "申请Argo$name隧道中……请稍等"
