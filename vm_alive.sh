0ca9a6bccf4afb112044d3aef5b17b8e96e216e9
KuizW9 <kuiz2sw@gmail.com>
2025-05-28 14:18:05 +0800

    Update vm_alive.sh

--- vm_alive.sh
+++ vm_alive.sh
@@ -2,7 +2,7 @@
 
 #bash <(curl -Ls https://raw.githubusercontent.com/KuizW9/argoSB-eooce/refs/heads/main/vm_alive.sh)
 
-# curl -o xui_login.py https://raw.githubusercontent.com/KuizW9/argoSB-eooce/refs/heads/main/xui_login.py
+# curl -o login.py https://raw.githubusercontent.com/KuizW9/argoSB-eooce/refs/heads/main/xui_login.py && chmod +x login.py
 # curl -o vm_alive.sh https://raw.githubusercontent.com/KuizW9/argoSB-eooce/refs/heads/main/vm_alive.sh && chmod +x vm_alive.sh && bash vm_alive.sh
 # masscan --exclude 255.255.255.255 -p54321,2053,1010,7777,12345,62000 --max-rate 5000 -oG results.txt 
 # masscan --exclude --banners 255.255.255.255 -p54321 --max-rate 5000 -oG results.txt -oJ scan.json 223.132.0.0/14
